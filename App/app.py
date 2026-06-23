dd64d3f53b25e325df1698a285ce32a9e9bdf0e9
Sujal Lothe <130586852+Sujal369@users.noreply.github.com>
2026-06-23 21:56:17 +0530

    Change bot to use webhook for updates
    Updated the bot to run on a webhook instead of polling.

--- App/app.py
+++ App/app.py
@@ -147,9 +147,16 @@ def main():
 
     app.add_handler(conv_handler)
 
-    print("🚀 Bot Running...")
+    print("🚀 Bot Running on Webhook...")
 
-    app.run_polling()
+    PORT = int(os.environ.get("PORT", 10000))
+    WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+    
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        webhook_url=WEBHOOK_URL
+    )
 
 
 if __name__ == "__main__":
